94753bb14cec1f737bf68a90ddc3b41af3b1ae42
KitarBit <nataredred@gmail.com>
2019-10-19 23:56:40 +0300

    init: solve the task

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const MORSE_TABLE = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
+
+function decode(expr) {
+   
+    while(expr.length / 10 != 0) {
+        let binarySymbol = expr.slice(0,10);
+        expr = expr.slice(10);
+        let index = binarySymbol.indexOf('1');
+        let morseLetter = "";
+        binarySymbol = binarySymbol.slice(index);
+
+        let morseLetter = "";
+        while (binarySymbol.length) {
+            let symbol = "";
+            switch(binarySymbol.slice(0,2)) {
+                case "10": 
+                    symbol = ".";
+                    break;
+                case "11": 
+                    symbol = "-";
+                    break;
+            }
+            morseLetter += symbol;
+            if (binarySymbol.length != 0) {
+                binarySymbol = binarySymbol.slice(2);
+            }            
+        }        
+        if(morseLetter.length) {
+            morseResult += MORSE_TABLE[morseLetter];
+        } else {
+            morseResult += " ";
+        }        
+    }
+    return morseResult;
+}
+
+module.exports = {
+    decode
+}
